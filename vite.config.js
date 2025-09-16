78fe69956992f37690cb6420d2b33a1cdd967702
arick-ws <a@arick.top>
2025-09-16 23:13:28 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    //vue()],
-  ]
+  build: {
+    rollupOptions: {
+      input: {
+        movie: 'movie.html',
+        player: 'player.html',
+        speech: 'speech.html'
+      }
+    }
+  }
 })
