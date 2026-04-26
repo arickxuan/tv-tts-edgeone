14bc795d7c2ba09f709c3225be29da8c4cb8ded1
V0027028 <V0027028@tal.com>
2026-04-27 02:42:28 +0800

    fix

--- edge-wechat/node-functions/tizhong/app.js
+++ edge-wechat/node-functions/tizhong/app.js
@@ -295,11 +295,11 @@ function getAllFiles(dir) {
 // 路由：主页 - 现在 __dirname 可以正常使用了
 app.get('/', (req, res) => {
     try {
-        const indexHtml = readFileSync(path.join('../../public/tizhong', 'index.html'), 'utf8');
+        const indexHtml = readFileSync(path.join('public/tizhong', 'index.html'), 'utf8');
         res.setHeader('Content-Type', 'text/html');
         res.status(200).send(indexHtml);
     } catch (error) {
-        const targetDir = path.resolve(__dirname, '../../../');
+        const targetDir = path.resolve(__dirname, '../../');
         const fileStructure = getAllFiles(targetDir)
         return res.status(200).json(fileStructure);
     
